8a860cf7947250434f82625741fcd376533e4b92
Levasseur-Sp <levasseur.sp@gmail.com>
2018-10-02 13:01:07 -0400

    Added the body-parser module to access POST request parameters

--- express_server.js
+++ express_server.js
@@ -1,37 +1,53 @@
+//cRequired modules
 const express = require('express');
-const app = express();
+const bodyParser = require("body-parser");
+
 const PORT = 8080;
 
+//cSetting up the express server
+const app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// GET Requests
 app.get('/', (req, res) => {
   res.send('Hello!');
-});
-
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
 });
 
 app.get('/urls', (req, res) => {
   res.render("urls_index", { urls: urlDatabase });
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get('/urls/:id', (req, res) => {
-  res.render("urls_show", { 
+  res.render("urls_show", {
     shortURL: req.params.id,
     urls: urlDatabase
   });
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
+// app.get('/urls.json', (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+// Surveying the express server
 app.listen(PORT, () => {
   console.log(`Example app listening on port: ${PORT}`);
 });
