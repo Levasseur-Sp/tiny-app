a5dfe50f4128a12a1e15fd7181c9941826289959
Levasseur-Sp <levasseur.sp@gmail.com>
2018-10-02 17:04:40 -0400

    Added a way to update to which url the shortURL is assigned to

--- express_server.js
+++ express_server.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Generates a URL of 6 randomized characters with each character's being one of 62 possibilities
-  // 56'800'235'584 possibilities
+// 56'800'235'584 possibilities
 function generateRandomString(lengthURL) {
   const possibleChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
   let randomURL = "";
@@ -52,16 +52,23 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 // Takes in the new Url to shorten and adds it to the 'database'
-app.post('/urls/', (req, res) => {
+app.post('/urls', (req, res) => {
   let shortURL = generateRandomString(6);
   res.redirect(`/urls/${shortURL}`);
   urlDatabase[shortURL] = req.body.longURL;
-  res.send(urlDatabase);
 });
 
 app.post('/urls/:id/delete', (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect(`/urls/`);
+});
+
+app.post('/urls/:id', (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.render("urls_show", {
+    shortURL: req.params.id,
+    urls: urlDatabase
+  });
 });
 
 
