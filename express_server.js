9f8ec81230f04915a4d70edad4d01cc4c868d491
Levasseur-Sp <levasseur.sp@gmail.com>
2018-10-03 11:01:48 -0400

    Added the cookie-parse module
    Added a 'login' cookie functionnality
    
    Added an update URL fonctionnality in the /urls page

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
-//cRequired modules
+// Required modules
 const express = require('express');
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+
 
 const PORT = 8000;
 
@@ -8,6 +10,7 @@ const PORT = 8000;
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Generates a URL of 6 randomized characters with each character's being one of 62 possibilities
 // 56'800'235'584 possibilities
@@ -32,7 +35,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  res.render("urls_index", { urls: urlDatabase });
+  res.render("urls_index", {
+    urls: urlDatabase,
+    username: req.cookies["user_name"]
+  });
 });
 
 app.get("/urls/new", (req, res) => {
@@ -40,9 +46,11 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get('/urls/:id', (req, res) => {
+  console.log('Updating!!')
   res.render("urls_show", {
     shortURL: req.params.id,
-    urls: urlDatabase
+    urls: urlDatabase,
+    username: req.cookies["user_name"]
   });
 });
 
@@ -51,7 +59,7 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-// Takes in the new Url to shorten and adds it to the 'database'
+// POST Requests
 app.post('/urls/', (req, res) => {
   let shortURL = generateRandomString(6);
   res.redirect(`/urls/${shortURL}`);
@@ -59,15 +67,24 @@ app.post('/urls/', (req, res) => {
 });
 
 app.post('/urls/:id/delete', (req, res) => {
+  console.log('deleting')
   delete urlDatabase[req.params.id];
   res.redirect('/urls/');
 });
 
 app.post('/urls/:id/update', (req, res) => {
+  console.log('updating')
   urlDatabase[req.params.id] = req.body.longURL;
+  console.log(req.body.longURL);
   res.redirect('/urls/')
 });
 
+app.post('/login', (req, res) => {
+  res.cookie('user_name', req.body.username);
+  res.render('site_login', {
+    username: req.cookies["user_name"]
+  });
+});
 
 // Surveying the express server
 app.listen(PORT, () => {
